71e5fc3e3185894ee93098de496d85e9e6d22cb7
nicoleribeiro <nicole.ribeiro1612@gmail.com>
2022-11-11 00:37:22 -0300

    encerrando palpites

--- mobile/src/components/Guesses.tsx
+++ mobile/src/components/Guesses.tsx
@@ -1,14 +1,100 @@
-import { Box } from 'native-base';
+import { useRoute } from '@react-navigation/native';
+import { Box, Code, FlatList, useToast } from 'native-base';
+import { useState, useEffect } from 'react';
+import { api } from '../services/api';
+import { EmptyMyPoolList } from './EmptyMyPoolList';
+import { Game, GameProps } from './Game';
+import { Loading } from './Loading';
+import { PoolPros } from './PoolCard';
 
 interface Props {
   poolId: string;
+  code: string;
 }
 
 export function Guesses({ poolId }: Props) {
+  const toast = useToast();
+  const [games, setGames] = useState<GameProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [firstTeamPoints, setFirstTeamPoints] = useState('');
+  const [secondTeamPoints, setSecondTeamPoints] = useState('');
+
+  async function fetchGames(){
+    try{
+      setIsLoading(true);
+
+      const response = await api.get(`/pools/${poolId}/games`);
+      setGames(response.data.games);
+    } catch(error){
+      console.log(error);
+
+      toast.show({
+          title: "Não foi possível carregar os detalhes os jogos",
+          placement: "top",
+          bgColor: "red.500",
+      })
+    } finally{
+        setIsLoading(false);
+    }
+  }
+
+  async function handleGuessConfirm(gameId: string){
+    try{
+      if(!firstTeamPoints.trim() || !secondTeamPoints.trim()){
+        return toast.show({
+          title: (!firstTeamPoints.trim() && secondTeamPoints.trim()) ? "Informe o placar do primeiro time" : (!secondTeamPoints.trim() && firstTeamPoints.trim()) ? "Informe o placar do segundo time" : "Informe o placar do palpite",
+          placement: "top",
+          bgColor: "red.500",
+      })
+      }
+
+      await api.post(`/pools/${poolId}/games/${gameId}/guesses`, {
+        firstTeamPoints: Number(firstTeamPoints),
+        secondTeamPoints: Number(secondTeamPoints),
+      })
+
+      toast.show({
+        title: "Palpite realizado com sucesso",
+        placement: "top",
+        bgColor: "green.500",
+    })
+
+    fetchGames();
+    }catch(error){
+      console.log(error);
+
+      toast.show({
+          title: "Não foi possível enviar o palpite",
+          placement: "top",
+          bgColor: "red.500",
+      })
+    }
+  }
+
+  useEffect(() => {
+    fetchGames();
+  }, [poolId])
+
+  if(isLoading){
+    return(
+      <Loading/>
+    )
+  }
 
   return (
-    <Box>
-
-    </Box>
-  );
+    <FlatList
+      data={games}
+      keyExtractor={item => item.id}
+      renderItem={({ item }) => (
+        <Game
+          data={item}
+          setFirstTeamPoints={setFirstTeamPoints}
+          setSecondTeamPoints={setSecondTeamPoints}
+          onGuessConfirm={() => handleGuessConfirm(item.id)}
+        />
+      )}
+      _contentContainerStyle={{pb: 10}}
+      ListEmptyComponent={() => <EmptyMyPoolList code={code} />}
+    />
+  )
 }
--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -28,7 +28,7 @@ export function AuthContextProvider({ children }: AuthProviderProps){
     const [isUserLoading, setIsUserLoading] = useState(false);
 
     const [request, response, promptAsync] = Google.useAuthRequest({
-        clientId: '332493459218-2gkqemanhe421firs2dh4lej5irju1ps.apps.googleusercontent.com',
+        clientId: process.env.CLIENT_ID,
         redirectUri: AuthSession.makeRedirectUri({ useProxy: true }),
         scopes: ['profile', 'email']
     })
